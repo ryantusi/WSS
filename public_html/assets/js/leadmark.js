e4ee31f6bcf8d3e3dba6b2e55366f9c5c99761c6
Ryan Tusi <ryantusi45@gmail.com>
2024-09-04 22:32:01 +0530

    certifications

--- public_html/assets/js/leadmark.js
+++ public_html/assets/js/leadmark.js
@@ -40,3 +40,30 @@ $(window).on("load", function() {
         }), !1
     })
 })
+
+// Membership Slider
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1200,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        touchMove: true,
+        swipe: true,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+  });
