f5ad46b04a68de2b075963118bea5ae25bd9d4c6
Ryan Tusi <ryantusi45@gmail.com>
2024-09-08 20:39:23 +0530

    map changes

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,42 +1,37 @@
 var map = L.map('map', {
-    zoomControl: false,         // Disable zoom control
-    scrollWheelZoom: false,     // Disable zoom by scroll
-    doubleClickZoom: false,     // Disable zoom by double-click
-    dragging: false,            // Disable dragging (panning)
+    zoomControl: false,
+    scrollWheelZoom: false,
+    doubleClickZoom: false,
+    dragging: false,
 }).setView([22, 0], 2);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 2,   // Lock zoom level
-    minZoom: 2,   // Lock zoom level
+    maxZoom: 2,
+    minZoom: 2,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map); 
+}).addTo(map);
 
-// Define fictional ship supply data per country with ISO codes
+// Define ship supply data per country with ISO codes
 var shipSupplyData = {
-    "USA": 500,
-    "CAN": 250,
-    "BRA": 120,
-    "GBR": 300,
-    "RUS": 400,
-    "IND": 600,
-    "CHN": 750,
-    "AUS": 200,
-    "ZAF": 150,
-    "FRA": 275,
-    "DEU": 325
+    "BHR": 151, "BGD": 151, "BRN": 51, "CHN": 501, "HKG": 51, "IND": 1001, "IDN": 501,
+    "KWT": 151, "MYS": 501, "MDV": 151, "MMR": 151, "OMN": 501, "PHL": 151, "QAT": 501,
+    "SAU": 501, "KOR": 151, "LKA": 501, "TWN": 251, "THA": 251, "TUR": 501, "ARE": 501,
+    "VNM": 151, "YEM": 151, "CYP": 51, "DNK": 51, "FRA": 51, "IRL": 51, "NLD": 51, 
+    "NOR": 51, "GBR": 51, "CAN": 51, "PAN": 151, "USA": 151, "BRA": 51, "EGY": 151, 
+    "ZAF": 51, "MAR": 51, "RUS": 151
 };
 
 // Function to assign color based on ship supply numbers
 function getColor(supplies) {
-    return supplies > 700 ? '#800026' :
-           supplies > 500 ? '#BD0026' :
-           supplies > 300 ? '#E31A1C' :
-           supplies > 200 ? '#FC4E2A' :
-           supplies > 100 ? '#FD8D3C' :
-                            '#FED976';
+    return supplies > 1000 ? '#800026' :
+           supplies > 500  ? '#BD0026' :
+           supplies > 250  ? '#E31A1C' :
+           supplies > 150  ? '#FC4E2A' :
+           supplies > 50   ? '#FD8D3C' :
+                             '#FED976';
 }
 
-// Style each country based on the ship supplies
+// Style each country based on ship supplies
 function style(feature) {
     var supplies = shipSupplyData[feature.properties.ISO_A3] || 0;  // Use ISO_A3 code to match data
     return {
@@ -99,7 +94,7 @@ info.onAdd = function (map) {
 // Update the info box on hover
 info.update = function (props) {
     this._div.innerHTML = '<h4>Ship Supplies Worldwide</h4>' + (props ?
-        '<b>' + props.ADMIN + '</b><br />' + (shipSupplyData[props.ISO_A3] || 0) + ' supplies'
+        '<b>' + props.ADMIN + '</b><br />'
         : 'Hover over a country');
 };
 
@@ -109,7 +104,7 @@ info.addTo(map);
 var legend = L.control({ position: 'bottomright' });
 legend.onAdd = function (map) {
     var div = L.DomUtil.create('div', 'info legend'),
-        grades = [0, 100, 200, 300, 500, 700],
+        grades = [0, 50, 150, 250, 500, 1000],
         labels = [];
 
     // Create a colored square for each supply range
